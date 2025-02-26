b227830e7bbdf40a23ccdc5e7727e2406c62a66f
Prathica Shetty M <123286880+PrathicaShettyM@users.noreply.github.com>
2025-02-26 13:30:00 +0530

    Added route protection and login and register functionality

--- tweetsapp/tweet/views.py
+++ tweetsapp/tweet/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from .models import Tweet
-from .forms import TweetForm
+from .forms import TweetForm, UserRegistrationForm
 from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+
+
 # Create your views here.
 
 def index(request):
@@ -11,6 +15,8 @@ def tweet_list(request):
     tweets = Tweet.objects.all().order_by('-created_at')
     return render(request, 'tweet_list.html', {'tweets': tweets})
 
+# decorators for route protection
+@login_required
 def tweet_create(request):
     if request.method == "POST": # check if the user has alrady filled the tweet
         form = TweetForm(request.POST, request.FILES) # extract data and file(if present)
@@ -26,6 +32,8 @@ def tweet_create(request):
     
     return render(request, 'tweet_form.html', {'form': form})
 
+# decorators for route protection
+@login_required
 def tweet_edit(request, tweet_id):
     tweet = get_object_or_404(Tweet, pk=tweet_id, user=request.user) #modelname,pk of which tweet, make sure he's able to edit only his tweet
     if request.method == 'POST': 
@@ -40,9 +48,25 @@ def tweet_edit(request, tweet_id):
         form = TweetForm(instance=tweet) # coz there will be data which is already stored
     return render(request, 'tweet_form.html', {'form': form})
 
+# decorators for route protection
+@login_required
 def tweet_delete(request, tweet_id):
     tweet = get_object_or_404(Tweet, pk=tweet_id, user=request.user)
     if request.method == 'POST':
         tweet.delete()
         return redirect('tweet_list')
     return render(request, 'tweet_confirm_delete.html', {'tweet': tweet})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False) # dont save it directly
+            user.set_password(form.cleaned_data['password1']) # clean the input to prevent sql injection attack
+            user.save()
+            login(request, user)
+            return redirect('tweet_list')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
